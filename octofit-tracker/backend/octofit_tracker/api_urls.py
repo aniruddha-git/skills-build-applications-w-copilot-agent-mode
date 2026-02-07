732bed7f8f5013159a8ba36ae578f4208209b34e
aniruddha-git <aniruddha.ghosh@hotmail.com>
2026-02-07 10:45:06 +0000

    Revert: Move API URLs back to root path from /api/ prefix

--- octofit-tracker/backend/octofit_tracker/api_urls.py
+++ octofit-tracker/backend/octofit_tracker/api_urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.http import JsonResponse
 from . import views
 from rest_framework.routers import DefaultRouter
 
@@ -9,7 +10,18 @@ router.register(r'activities', views.ActivityViewSet)
 router.register(r'workouts', views.WorkoutViewSet)
 router.register(r'leaderboard', views.LeaderboardViewSet)
 
+def api_url_list(request):
+    base = '/api/'
+    return JsonResponse({
+        'users': base + 'users/',
+        'teams': base + 'teams/',
+        'activities': base + 'activities/',
+        'workouts': base + 'workouts/',
+        'leaderboard': base + 'leaderboard/',
+    })
+
 urlpatterns = [
     path('', views.api_root, name='api-root'),
+    path('get', api_url_list, name='api-url-list'),
     path('api/', include(router.urls)),
 ]
